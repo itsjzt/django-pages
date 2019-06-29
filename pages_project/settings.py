f2bccbb43c44d760cf2e2c0363462ae13fadd39e
Saurabh Sharma <saurabh.19@outlook.com>
2019-06-29 18:58:38 +0530

    added deploy settings

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_zb+gy=x2a3elpi7o72m@yqf4cx*&q8+lu2q)7)8$!hd)p^r5%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
